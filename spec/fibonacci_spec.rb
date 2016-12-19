426fd02ce21f5c7c92facf6676a3e22da8ede93d
qczhang18 <aznyqjai@gmail.com>
2016-12-19 17:13:14 -0500

    frame

--- spec/fibonacci_spec.rb
+++ spec/fibonacci_spec.rb
@@ -1,6 +1,13 @@
-require_relative '../lib/lambda'
+require_relative '../lib/fibonacci'
 require 'byebug'
 
-RSpec.describe "MyLambda" do
+RSpec.describe "Fibonacci" do
+  before(:each) do
+    @fibonacci = Fibonacci.new
+  end
+
+  it "should initialize" do
+    expect(@fibonacci.class).to eq(Fibonacci)
+  end
 
 end
